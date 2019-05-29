f74d66640797e729364054b305fcd717347cc863
unknown <siwary@naver.com>
2019-05-29 11:42:46 +0900

    variables

---
+++ day1/go-basic/1.variables/variables.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	var a = "this is a"
+
+	fmt.Println(a)
+
+	// (:=) : 변수선언 + 초기값셋팅
+	b := "this is b"
+
+	fmt.Println(b)
+
+	// (=) : 변수값셋팅
+	b = "this is c"
+
+	fmt.Println(b)
+
+}
